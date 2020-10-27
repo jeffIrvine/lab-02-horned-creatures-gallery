89588ea058cba73112a1e4a1d997538945e35a3e
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-10-27 16:42:49 -0700

    filter function working

--- src/ImageList.js
+++ src/ImageList.js
@@ -3,20 +3,48 @@ import images from './Data.js'
 import ImageListItem from './ImageListItem.js'
 
 export default class ImageList extends Component {
+    state= {
+        filter: '',
+    }
+
+    handleChange = e => {
+        this.setState({
+            filter: Number(e.target.value)
+        });
+    }
+
     render() {
+
+        const filterData = images.filter((item) => {
+            if (!this.state.filter) return true;
+
+            if (item.horns === this.state.filter) return true;
+
+            return false
+        }); 
+
         return (
-            <div className='image-div'>
+
+            <div >
+                <>
+                <select onChange={this.handleChange}>
+                    <option value=''>Show All</option>
+                    <option value='1'>1 Horn</option>
+                    <option value='2'>2 Horns</option>
+                    <option value='3'>3 Horns</option>
+                    <option value='100'>100 Horns</option>
+                </select>
                 {
-                    images.map((item, i) => {
+                    filterData.map((item) => {
                         return <ImageListItem 
                         url={item.url}
-                        alt={item.title}
-                        title={item.title} 
-                        key={i}
+                        title={item.title}
+                        booger={item.description}
+                        horns={item.horns}
                         />
                     })
-
                 }
+                </>
             </div>
         )
     }
--- src/ImageListItem.js
+++ src/ImageListItem.js
@@ -3,14 +3,20 @@ import React, { Component } from 'react'
 export default class ImageListItem extends Component {
     render() {
         return (
-            <>
             <div className='image-div'>
-                <h1 className='image-title'>
+                <h1>
                     {this.props.title}
                 </h1>
-                <img className='horned-images' src={this.props.url} alt={this.props.title}/>
+                <img className='horned-images' 
+                src={this.props.url} 
+                alt={this.props.title} />
+                <p>
+                    {this.props.booger}
+                </p>
+                <p>
+                   Number of horns: {this.props.horns}
+                </p>
             </div>
-            </>
         )
     }
 }
